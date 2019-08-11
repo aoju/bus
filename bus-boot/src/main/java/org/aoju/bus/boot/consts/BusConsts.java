95ac7f9b4143916430c70438019d2567ecccf394
Kimi Liu <839536@QQ.COM>
2019-08-11 17:21:10 +0800

    [更新] v3.0.0

---
+++ bus-boot/src/main/java/org/aoju/bus/boot/consts/BusConsts.java
@@ -0,0 +1,54 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2017, aoju.org All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.aoju.bus.boot.consts;
+
+
+/**
+ * 全局常量配置
+ *
+ * @author Kimi Liu
+ * @version 3.0.0
+ * @since JDK 1.8
+ */
+public class BusConsts {
+
+    /***
+     * 应用名: 备注 @Value("${spring.application.name:@null}")
+     */
+    public static final String BUS_APP_NAME = "spring.application.name";
+
+    /***
+     * 应用版本号
+     */
+    public static final String BUS_BOOT_VERSION = "bus-boot.version";
+    /***
+     * 应用版本信息
+     */
+    public static final String BUS_BOOT_FORMATTED_VERSION = "bus-boot.formatted-version";
+    /***
+     * 应用属性
+     */
+    public static final String BUS_BOOT_PROPERTIES = "BusConfigurationProperties";
+
+}
