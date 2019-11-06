3431d1238e227de21102616dbf5fdd2a2413e425
Kimi Liu <839536@qq.com>
2019-11-06 13:27:51 +0800

    v5.1.0

---
+++ bus-spring/src/main/java/org/aoju/bus/spring/annotation/EnableDubbo.java
@@ -0,0 +1,79 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2017 aoju.org All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.aoju.bus.spring.annotation;
+
+import com.alibaba.dubbo.config.spring.context.annotation.DubboComponentScan;
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubboConfig;
+import org.aoju.bus.spring.dubbo.DubboConfiguration;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.annotation.AliasFor;
+
+import java.lang.annotation.*;
+
+/**
+ * Dubbo 支持
+ *
+ * @author Kimi Liu
+ * @version 5.1.0
+ * @since JDK 1.8+
+ */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@Documented
+@EnableDubboConfig
+@DubboComponentScan
+@Import(DubboConfiguration.class)
+public @interface EnableDubbo {
+
+    /**
+     * Base packages to scan for annotated @Service classes.
+     *
+     * @return the base packages to scan
+     * @see DubboComponentScan#basePackages()
+     */
+    @AliasFor(annotation = DubboComponentScan.class, attribute = "basePackages")
+    String[] basePackage() default {};
+
+    /**
+     * packages to scan for annotated @Service classes.
+     * The package of each class specified will be scanned.
+     *
+     * @return classes from the base packages to scan
+     * @see DubboComponentScan#basePackageClasses
+     */
+    @AliasFor(annotation = DubboComponentScan.class, attribute = "basePackageClasses")
+    Class<?>[] basePackageClass() default {};
+
+
+    /**
+     * binding to multiple Spring Beans.
+     *
+     * @return the default value is <code>true</code>
+     * @see EnableDubboConfig#multiple()
+     */
+    @AliasFor(annotation = EnableDubboConfig.class, attribute = "multiple")
+    boolean multiple() default true;
+
+}
