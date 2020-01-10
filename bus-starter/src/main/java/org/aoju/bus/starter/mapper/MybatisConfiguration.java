b9c791d7822afe56ba28442db74456d9f1593c09
Justubborn <wubenhui@gmail.com>
2020-01-10 15:06:44 +0800

    修复没有mapper xml文件启动报错的问题

--- bus-starter/src/main/java/org/aoju/bus/starter/mapper/MybatisConfiguration.java
+++ bus-starter/src/main/java/org/aoju/bus/starter/mapper/MybatisConfiguration.java
@@ -26,6 +26,7 @@ package org.aoju.bus.starter.mapper;
 
 import org.aoju.bus.core.lang.exception.InstrumentException;
 import org.aoju.bus.core.utils.StringUtils;
+import org.aoju.bus.logger.Logger;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
@@ -36,6 +37,7 @@ import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.core.io.support.ResourcePatternResolver;
 
 import javax.sql.DataSource;
+import java.io.FileNotFoundException;
 
 /**
  * mybatis 配置
@@ -61,8 +63,11 @@ public class MybatisConfiguration {
 
             ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
             bean.setPlugins(MybatisPluginBuilder.build(properties));
-
-            bean.setMapperLocations(resolver.getResources(this.properties.getXmlLocation()));
+            try {
+                bean.setMapperLocations(resolver.getResources(this.properties.getXmlLocation()));
+            } catch (FileNotFoundException e) {
+                Logger.warn(e.getMessage());
+            }
             return bean.getObject();
         } catch (Exception e) {
             throw new InstrumentException(e);
