7f33df9254626f143f3fcee2460f0523e7586499
839536 <839536@qq.com>
2020-04-14 10:20:15 +0800

    v5.8.5

---
+++ bus-notify/src/main/java/org/aoju/bus/notify/Builder.java
@@ -0,0 +1,79 @@
+/*********************************************************************************
+ *                                                                               *
+ * The MIT License                                                               *
+ *                                                                               *
+ * Copyright (c) 2015-2020 aoju.org and other contributors.                      *
+ *                                                                               *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy  *
+ * of this software and associated documentation files (the "Software"), to deal *
+ * in the Software without restriction, including without limitation the rights  *
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell     *
+ * copies of the Software, and to permit persons to whom the Software is         *
+ * furnished to do so, subject to the following conditions:                      *
+ *                                                                               *
+ * The above copyright notice and this permission notice shall be included in    *
+ * all copies or substantial portions of the Software.                           *
+ *                                                                               *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR    *
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,      *
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE   *
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER        *
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, *
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN     *
+ * THE SOFTWARE.                                                                 *
+ ********************************************************************************/
+package org.aoju.bus.notify;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * 构造信息
+ *
+ * @author Kimi Liu
+ * @version 5.8.5
+ * @since JDK 1.8+
+ */
+@Setter
+public class Builder {
+
+
+    private Builder() {
+
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public enum Status {
+
+        SUCCESS("0", "Success"),
+        FAILURE("-1", "Failure");
+
+        private String code;
+        private String msg;
+
+        }
+
+    /**
+     * 缓存类型
+     */
+    @Getter
+    @ToString
+    public enum Type {
+        /**
+         * 使用内置的缓存
+         */
+        DEFAULT,
+        /**
+         * 使用Redis缓存
+         */
+        REDIS,
+        /**
+         * 自定义缓存
+         */
+        CUSTOM
+    }
+
+}
