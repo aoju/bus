0dccf009c45385845a2134fcdf8def1873c01a42
839536 <839536@qq.com>
2020-07-06 17:13:35 +0800

    fix add Journal

---
+++ bus-core/src/main/java/org/aoju/bus/core/annotation/Journal.java
@@ -0,0 +1,68 @@
+/*********************************************************************************
+ *                                                                               *
+ * The MIT License (MIT)                                                         *
+ *                                                                               *
+ * Copyright (c) 2015-2020 aoju.org and other contributors.                      *
+ *                                                                               *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy  *
+ * of this software and associated documentation files (the "Software"), to deal *
+ * in the Software without restriction, including without limitation the rights  *
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell     *
+ * copies of the Software, and to permit persons to whom the Software is         *
+ * furnished to do so, subject to the following conditions:                      *
+ *                                                                               *
+ * The above copyright notice and this permission notice shall be included in    *
+ * all copies or substantial portions of the Software.                           *
+ *                                                                               *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR    *
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,      *
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE   *
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER        *
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, *
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN     *
+ * THE SOFTWARE.                                                                 *
+ ********************************************************************************/
+package org.aoju.bus.core.annotation;
+
+import org.aoju.bus.core.lang.Normal;
+
+import java.lang.annotation.*;
+
+/**
+ * 操作记录
+ *
+ * @author Kimi Liu
+ * @version 5.9.9
+ * @since JDK 1.8+
+ */
+@Target({ElementType.PARAMETER, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface Journal {
+
+    /**
+     * @return 标题
+     */
+    String value() default Normal.EMPTY;
+
+    /**
+     * @return 模块
+     */
+    String module() default Normal.EMPTY;
+
+    /**
+     * @return 业务
+     */
+    String business() default Normal.EMPTY;
+
+    /**
+     * @return 操作人类别
+     */
+    String operator() default Normal.EMPTY;
+
+    /**
+     * @return 是否保存请求的参数
+     */
+    boolean isSaveRequest() default true;
+
+}
