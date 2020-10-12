cc7582e4d2dd52fc85f81be3332d2985b6c1786d
Sixawn.Zheng <congchun.zheng@gmail.com>
2020-10-12 14:14:41 +0800

    .feat: add x_remote_channel, x_remote_terminal, x_remote_browser

--- bus-base/src/main/java/org/aoju/bus/base/entity/Tracer.java
+++ bus-base/src/main/java/org/aoju/bus/base/entity/Tracer.java
@@ -70,4 +70,22 @@ public class Tracer extends OAuth2 {
     @Transient
     protected String x_remote_ip;
 
+    /**
+     * 请求者渠道类型: 1-WEB, 2-APP, 3-钉钉，4-微信小程序，5-其他；
+     */
+    @Transient
+    protected String x_remote_channel;
+
+    /**
+     * 请求者终端类型: 1-PC, 2-Android, 3-iPhone, 4-iPad, 5-WinPhone, 6-HarmonyOS，7-其他
+     */
+    @Transient
+    protected String x_remote_terminal;
+
+    /**
+     * 请求者浏览器信息: APP 原生则传系统版本
+     */
+    @Transient
+    protected String x_remote_browser;
+
 }
